1048a010fcee8147e7b3fb3a33f66ad16a4bb92e
SahilMadan <Hillatio@gmail.com>
2020-04-12 00:59:39 -0700

    Initial commit for openctn
    This includes the default boilerplate from a new React project.

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          openctn
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/SahilMadan/openctn"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github Link
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
