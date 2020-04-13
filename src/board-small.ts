c72c6f3b8a78ff3700590fa85d6d73d70a1bf53a
Farbod Motlagh <farbod.motlagh@gmail.com>
2020-04-12 22:52:21 -0700

    hardcoded 4v4 map

--- src/board-small.ts
+++ src/board-small.ts
@@ -1,5 +1,7 @@
 import {IBoard} from './board-interface';
-import {Tile} from './tile'
+import {TileType, TileParams, Tile} from './tile'
+import {TileEdge} from './tile-edge'
+import {TileCorner} from './tile-corner'
 
 export class BoardSmall implements IBoard {
   static readonly AmountTiles = 19;
@@ -8,10 +10,94 @@ export class BoardSmall implements IBoard {
 
   constructor() {
     this.tiles = [];
-    for (let i = 0; i < BoardSmall.AmountTiles; ++i) {
-      this.tiles.push(new Tile(null));
-    }
+    this.generateMap();
     this.positionTiles();
+  }
+
+  generateMap() {
+    // Hard coded 4v4 map.
+    let tilesPerRow = [3, 4, 5, 4, 3];
+    let tiles = [];
+    tilesPerRow.forEach((numTiles, rowIndex) => {
+      let tilesInRow = [];
+      for (let i = 0; i < numTiles; i++) {
+        let edges : TileEdge[] = [];
+        let corners : TileCorner[] = [];
+        // Create new edges and corners for the [rowIndex, 0] tile.
+        if (i == 0) {
+          for (let j = 0; j < 6; j++) {
+            // 0-th and 1st corners are shared with the tile in the previous row (4th and 3rd corners).
+            if (rowIndex > 0 && (j == 0 || j == 1)) {
+              let prevRowTile = tiles[tiles.length - 1][0];
+              corners.push(prevRowTile.corners[(j == 0) ? 4 : 3]);
+            } else {
+              corners.push(new TileCorner());
+            }
+          }
+          for (let j = 0; j < 6; j++) {
+            // 0-th edge is shared with the tile in the previous row (3rd edge).
+            if (rowIndex > 0 && j == 0) {
+              let prevRowTile = tiles[tiles.length - 1][0];
+              edges.push(prevRowTile.edges[3]);
+            } else {
+              let newEdge = new TileEdge(corners[j], corners[(j == 5) ? 0 : (j+1)]);
+              edges.push(newEdge);
+              corners[j].tileEdges.push(newEdge);
+              corners[(j == 5) ? 0 : (j+1)].tileEdges.push(newEdge);
+            }
+          }
+        } else {
+          // Everything else to the right of it shares two corners and an edge with the previous tile.
+          let prevTile = this.tiles[this.tiles.length - 1];
+          for (let j = 0; j < 6; j++) {
+            if (rowIndex > 0 && (j == 0 || j == 1) && (tiles[tiles.length - 1].length < i)) {
+              // 0-th and 1st corners are shared with the tile in the previous row (4th and 3rd corners).
+              let prevRowTile = tiles[tiles.length - 1][i];
+              corners.push(prevRowTile.corners[(j == 0) ? 4 : 3]);
+            } else if (j == 5) {
+              // 1st corner of previous tile is shared with 5th corner of the new tile.
+              corners.push(prevTile.corners[1]);
+            } else if (j == 4) {
+              // 2nd corner of previous tile is shared with 4th corner of the new tile.
+              corners.push(prevTile.corners[2]);
+            } else {
+              corners.push(new TileCorner());
+            }
+          }
+
+          // Similarly 1st edge of the new tile is shared with 4th edge of the new tile.
+          for (let j = 0; j < 6; j++) {
+            if (rowIndex > 0 && (j == 5)) {
+              // 5th edge is shared with the 2nd edge of the tile in the previous row (to its right).
+              let prevRowTile = tiles[tiles.length - 1][i-1];
+              edges.push(prevRowTile.edges[2]);
+            } else if (rowIndex > 0 && (j == 0) && (tiles[tiles.length - 1].length < i)) {
+              // 0th edge is shared with the 3rd edge of the tile in the previous row (to its left).
+              let prevRowTile = tiles[tiles.length - 1][i];
+              edges.push(prevRowTile.edges[3]);
+            } else if (j == 4) {
+              // 4th edge is shared with the 1st of the previous tile.
+              edges.push(prevTile.edges[1]);
+            } else {
+              let newEdge = new TileEdge(corners[j], corners[(j == 5) ? 0 : (j+1)]);
+              edges.push(newEdge);
+              corners[j].tileEdges.push(newEdge);
+              corners[(j == 5) ? 0 : (j+1)].tileEdges.push(newEdge);
+            }
+          }
+        }
+        let params : TileParams = {
+          // TODO: Fix tile type.
+          tileType: TileType.DESERT,
+          cornerList: corners,
+          edgeList : edges,
+        }
+        let newTile = new Tile(params);
+        this.tiles.push(newTile);
+        tilesInRow.push(newTile);
+      }
+      tiles.push(tilesInRow);
+    });
   }
 
   positionTiles() {
--- src/tile.ts
+++ src/tile.ts
@@ -1,5 +1,6 @@
 import {TileCorner} from './tile-corner';
 import {TileEdge} from './tile-edge';
+import {assert} from './util/error';
 
 export enum TileType {
   DESERT,
@@ -10,23 +11,30 @@ export enum TileType {
   BRICK,
 }
 
+export interface TileParams {
+  tileType: TileType;
+  cornerList: TileCorner[];
+  edgeList: TileEdge[];
+}
+
 export class Tile {
   static readonly Height = 50;
   static readonly Width = 50;
 
   // Game properties.
   readonly tileType : TileType;
-  private tileCornerList : TileCorner[];
-  private tileEdgeList : TileEdge[];
+  readonly corners: TileCorner[];
+  readonly edges: TileEdge[];
 
   // Render properties.
   public xPos : number;
   public yPos : number;
 
-  constructor(tileType: TileType) {
-    this.tileType = tileType;
-    this.tileCornerList.fill
-    // this.tileType = tileParams.tileType;
-    // this.landList = tileParams.landList; 
+  constructor(params: TileParams) {
+    this.tileType = params.tileType;
+    assert(params.cornerList.length == 6, "Number of corners must be 6.")
+    assert(params.edgeList.length == 6, "Number of edges must be 6.")
+    this.corners = params.cornerList;
+    this.edges = params.edgeList;
   }
 };
