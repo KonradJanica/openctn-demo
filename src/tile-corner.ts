89a20010ca3db81e94b40f0ebb24571c70caab87
SahilMadan <Hillatio@gmail.com>
2020-04-12 20:50:21 -0700

    Add skeleton corner state code to tile-corner to keep track of current state.

--- src/tile-corner.ts
+++ src/tile-corner.ts
@@ -1,9 +1,27 @@
+import {Player} from './player'
 import {TileEdge} from './tile-edge'
 
+export enum TileCornerType {
+  EMPTY,
+  SETTLEMENT,
+  CITY
+}
+
+export interface TileCornerState {
+  cornerType: TileCornerType;
+  // Specified if cornerType is SETTLEMENT or CITY
+  owner: Player|null;
+}
+
 export class TileCorner {
+  private readonly cornerState: TileCornerState;
   private readonly tileEdges: TileEdge[];
 
   constructor(edges: TileEdge[]) {
     this.tileEdges= edges;
+  }
+
+  State(): TileCornerState {
+    return Object.assign({}, this.cornerState);
   }
 }
